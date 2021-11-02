a261c3dccbb2de192e40101c26df4e1bf1045913
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-11-02 23:30:25 +0530

    added buttons logo started working on footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from 'next/image';
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -11,18 +15,38 @@ export default function Home() {
       </Head>
 
       <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+
         <div className="flex space-x-4 items-center"></div>
 
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
           <Avatar url="https://pbs.twimg.com/profile_images/1449022166930919432/6YdCe7WM_400x400.jpg"/>
         </div>
+
       </header>
 
-      {/* Body */}
+<form className="flex flex-col items-center mt-44 flex-grow">
+<Image src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width={300} height={100} />
 
-      {/* Footer */}
+<div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+  <SearchIcon className="h-5 ml-3 text-gray-500"/>
+  <input type="text" className="flex-grow focus:outline-none"></input>
+  <MicrophoneIcon className="h-5"/>
+</div>
+
+<div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+  <button className="btn">Google Search</button>
+  <button className="btn">I'm Feeling Lucky</button>
+</div>
+
+</form>
+
+<Footer/>
+      
     </div>
+
+    
   );
 }
