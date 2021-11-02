e8c725991dee837142036f3a8580537f9437d6e6
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-11-02 22:24:44 +0530

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <div>
       <Head>
-        <title>Google Clone</title>
-        <meta name="description" content="Google Clone" />
+        <title>Google</title>
+        <meta name="description" content="Google" />
         <link rel="icon" href="https://www.google.com/favicon.ico" />
       </Head>
 
-     
-      
-  )
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center"></div>
+
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+        </div>
+      </header>
+
+      {/* Body */}
+
+      {/* Footer */}
+    </div>
+  );
 }
