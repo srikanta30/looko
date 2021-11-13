89b6a76b99efff7bb6364a08303b9ecc5fa51782
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-11-13 21:58:06 +0530

    updated History

--- frontend/src/components/History/History.jsx
+++ frontend/src/components/History/History.jsx
@@ -4,6 +4,7 @@ import BottomNav from "../Utils/BottomNav";
 import forward from './icons/forward.svg';
 import searchicon from './icons/searchicon.svg'
 import './history.css'
+import demoimage from './demoimage.jpg';
 
 function History() {
   return (
@@ -17,38 +18,38 @@ function History() {
                 <input type="text" placeholder="Search" className="search" />
               </td>
               <td>
-                <a href="#"><img src={searchicon} /></a>
+                <a href="#"><img src={searchicon} alt=""/></a>
               </td>
             </tr>
           </table>
         </div>
         <div id="card-container">
           <div className="card">
-            <img className="pfpimg" src="https://4.imimg.com/data4/EH/NN/MY-17368652/boys-casual-shirt-500x500.jpg" />
+            <img className="pfpimg" src={demoimage} alt="" />
             <div className = "content">
               <h5>Check your entire look</h5>
-              <div>date : </div>
-              <div>Time : </div>
+              <div>Date : 13-11-2021 </div>
+              <div>Time : 07:53 PM </div>
             </div>
-            <img src={forward} />
+            <img src={forward} alt="" />
           </div>
           <div className="card">
-            <img className="pfpimg" src="https://4.imimg.com/data4/ET/HI/MY-17368652/boys-designer-shirt-500x500.jpg" />
+            <img className="pfpimg" src="https://4.imimg.com/data4/ET/HI/MY-17368652/boys-designer-shirt-500x500.jpg" alt=""/>
             <div className = "content">
               <h5>Check your entire look</h5>
-              <div>date : </div>
-              <div>Time : </div>
+              <div>Date : 13-11-2021 </div>
+              <div>Time : 12:50 PM </div>
             </div>
-            <img src={forward} />
+            <img src={forward} alt=""/>
           </div>
           <div className="card">
-            <img className="pfpimg" src="https://m.media-amazon.com/images/I/71e+PwfDV8L._UL1500_.jpg" />
+            <img className="pfpimg" src="https://m.media-amazon.com/images/I/71e+PwfDV8L._UL1500_.jpg" alt=""/>
             <div className = "content">
               <h5>Check your entire look</h5>
-              <div>date : </div>
-              <div>Time : </div>
+              <div>Date : 13-11-2021</div>
+              <div>Time : 10:50 AM </div>
             </div>
-            <img src={forward} />
+            <img src={forward}  alt=""/>
           </div>
         </div>
       </div>
