6d17f6a0e7c057f647d5dbdefb8fd4af3a6cfad8
Srikanta Banerjee <srikantabanerjee30@gmail.com>
2021-11-13 17:01:39 +0530

    Gallery Done

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -1,13 +1,17 @@
 import { Header } from "../Utils/Header";
 import Button from "../Utils/Button";
 import BottomNav from "../Utils/BottomNav";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useCallback} from "react";
 import "./gallery.css";
+import spinner from "./spinner.gif";
+import Results from "../Results/Results";
 
 function Gallery() {
 
     const [selectedFile, setSelectedFile] = useState()
-    const [preview, setPreview] = useState()
+    const [preview, setPreview] = useState();
+    const [result, setResult] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     
     useEffect(() => {
@@ -32,20 +36,43 @@ function Gallery() {
         setSelectedFile(e.target.files[0])
     }
 
+    const handleResult = useCallback(() => {
+        setResult(true);
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+      }, [setResult, setLoading]);
+
 
 
     return (
+        <>
+          <div>{ result ? ( loading ? (
+              <div className="loader">
+                <img alt="loading" src={spinner} />
+              </div>
+            ) : (
+                <div className="resultsdiv">
+                  <Header/>
+                  <img src={preview} alt="Captured_Photo" className="resultsphoto" />
+                  <Results />
+                </div>
+              )) : (
+
         <div>
             <Header />
         <div className="centergallery">
             
             <input type='file' onChange={onSelectFile} />
             {selectedFile &&  <img src={preview} alt="preview"/> }
-            <Button text="SHOW ME THE RESULT"/>
+            <Button text="SHOW ME THE RESULT" handleClick={handleResult}/>
         </div>
         <BottomNav />
         </div>
-    )
+    )}
+    </div>
+    </>
+  )
 }
 
 export default Gallery
